c57b374128a676ff479a993079423578c9f8932b
Brad Magyar <brad.magyar@morelandconnect.com>
2019-10-30 00:38:36 -0400

    Added license

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ package_name=${package_split[-1]}
 
 package_shortname=$(echo $package_name | sed --expression='s/.go//g')
 
-platforms=("linux/amd64" "linux/386" "linux/arm" "linux/arm64" "netbsd/amd64" "netbsd/386" "netbsd/arm" "openbsd/amd64" "openbsd/386" "openbsd/arm" "windows/amd64" "windows/386" "darwin/amd64" "darwin/386" "darwin/arm" "darwin/arm64")
+platforms=("linux/amd64" "linux/386" "linux/arm" "linux/arm64" "windows/amd64" "windows/386" "darwin/amd64" "darwin/386" "darwin/arm64")
 
 for platform in "${platforms[@]}"
 do
